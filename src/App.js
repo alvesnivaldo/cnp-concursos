2973062b81724adf064f37e337f05ea465c401ea
alvesnivaldo <alvesnivaldo204@gmail.com>
2026-06-08 22:21:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -564,7 +564,7 @@ if (tela === "confirmar") {
                 style={{
                   position: "absolute",
                   top: "200px", 
-                  left: "1600px",              
+                  right: "20px",              
                   background: "#fff",
                   color: "#333",
                   padding: "8px 12px",
